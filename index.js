c9ae7c28036aa3e225ec075d37e42eabfffa0ddb
Isabelle Ferreira <isabelle.a.ferreira@gmail.com>
2020-05-06 15:39:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,24 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+var itemPrice = Math.floor(Math.random() *(100 - 1)+ 1);
+cart.push({itemName: item, itemPrice: itemPrice});
+return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+var words1 = 'In your cart, you have'
+if (cart.length === 0) {
+  return 'Your shopping cart is empty.'
+}
+else if (cart.length === 1) {
+  return `${words1} ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`
+}
+else if (cart.length === 2) {
+  return `${words1} ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`
+}
+
+
 }
 
 function total() {
