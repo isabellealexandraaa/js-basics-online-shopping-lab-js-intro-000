fa75f080e80faab426125aa202d099ed22c27259
Isabelle Ferreira <isabelle.a.ferreira@gmail.com>
2020-05-06 16:01:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ else if (cart.length === 2) {
   return `${words1} ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`
 }
 else if (cart.length >= 3) {
-  var words2 = ''
-  for (var i = 0; i < cart.length-1; i++) {
-    words2 += `${getCart()[i].itemName} at $${getCart()[i].itemPrice},`
-  }
-  var words3 =`and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`
-  return `${words1}  ${words2} ${words3}.`
+        var words2 = ''
+      for (var i = 0; i < cart.length-1; i++) {
+        words2 += `${getCart()[i].itemName} at $${getCart()[i].itemPrice}, `
+      }
+        var words3 =`and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`
+      return `${words1} ${words2}${words3}.`
 }
 
 }
